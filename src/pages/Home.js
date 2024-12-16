2c7ba05d53b8b0a6c130eb1b44dab9256f7f65e7
Ridzz23 <rsrikanth@ucsd.edu>
2024-12-15 21:01:59 -0800

    added links to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import Navbar from '../Navbar';
+import Navbar from '../components/Navbar';
 import '../App.css';
 
 
 const Home = () => {
+
+  const openLink = (link) => {
+    console.log(link);
+    window.open(link, "_blank");
+  }
+
   return (
     <div>
       <Navbar />
-      <div
-        className="container left-align"
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
-      >
-        <h1 className="App-header">
+
+        <h1 className="child-left">
           Hello World!<br></br>
           I'm Ridhi.
           </h1>
-      </div>
+          <div class="child-right">
+            <div class="button-container">
+              <button class="button1" type="button" onClick={() => openLink("https://www.linkedin.com/in/ridhi-srikanth-8495a6222/")}>LinkedIn</button>
+              <button class="button1" type="button" onClick={() => openLink("https://github.com/Ridzz23")}>Github</button>
+            </div>
+          </div>
     </div>
   );
 };
