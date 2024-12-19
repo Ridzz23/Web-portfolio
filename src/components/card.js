4dbe06aedaecb630ef585f6ae36c70c2188b647a
Ridzz23 <rsrikanth@ucsd.edu>
2024-12-19 13:40:25 -0800

    card content

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../App.css';
 
-const Card = ({header, content, image, linkName, link}) => {
+const Card = ({header, content1, content2, image, linkName, link}) => {
 
     return(
         <div className="card">
             <div className="card-header">{header}</div>
             <img className="card-image" src={image} alt="card image" />
-            <div className="card-content">{content}</div>
+            <div className="card-content">{content1}</div>
+            <br></br>
+            <div className="card-content">{content2}</div>
             <a className="card-link" href={link} target="_blank" rel="noopener noreferrer">{linkName}</a>
         </div>
 
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -20,7 +20,7 @@ const Experience = () => {
     </div>
     <hr></hr>
     <div className="cards-container">
-    <Card header="LabStream" content="yellow mellow" image={githubImg} linkName = "Here" link="https://github.com/Ridzz23" />
+    <Card header="LabStream" content1="Collaborating with a team of 8 developers under the guidance of Professor Saharnaz Baghdadchi to develop a virtual lab platform for the Electrical and Computer Engineering department at UCSD. The web application enables students to control lab equipment and conduct experiments through an interactive React-based UI, with real-time feedback provided via a live camera feed streamed through WebSockets. Student inputs, such as increasing motor speed, are processed through a Flask backend, where Python automation scripts communicate with ESP32 microcontrollers to execute commands in the lab setup. This project combines hardware automation and software development to create a hands-on, remote learning experience for students." content2="hello" image={githubImg} linkName = "Here" link="https://github.com/Ridzz23" />
     </div>
     </div>
   );
